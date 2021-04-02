9288a8a397b6888d37b1ff58343972ffb573e3c7
Volkan Karakus <64658867+volkaankarakus@users.noreply.github.com>
2021-04-02 15:18:01 +0300

    Add files via upload

---
+++ String/trimLtrimRtrim.sql
@@ -0,0 +1,6 @@
+--TRIM,LTRIM,RTRIM
+--bosluk temizler
+
+SELECT TRIM(' VOLKAN     KARAKUS     ')--basi ve sonu temizler
+SELECT LTRIM(' VOLKAN     KARAKUS     ')--basini temizler
+SELECT RTRIM(' VOLKAN     KARAKUS     ')--sonunu temizler
