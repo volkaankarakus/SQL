4e65085814d01e5291fe6cdb386aaf15b1fb75a1
Volkan Karakus <64658867+volkaankarakus@users.noreply.github.com>
2021-04-02 15:02:43 +0300

    Add files via upload

---
+++ String/format.sql
@@ -0,0 +1,8 @@
+--FORMAT
+
+SELECT FORMAT(GETDATE(),'d','en-us')--GETDATE(), bugunun tarihini getirir.
+-- ingilterede önce ay, sonra gun, sonra yil.
+
+SELECT FORMAT(GETDATE(),'D','en-us')
+
+--diger formatlara microsoft'un kendi sitesinden bak.
