a2c80dd1ad8cb252215f0619d91fc04211eefd62
Volkan Karakus <64658867+volkaankarakus@users.noreply.github.com>
2021-04-05 02:11:38 +0300

    Add files via upload

---
+++ Senaryo1-MusteriVerisi/exmp8.sql
@@ -0,0 +1,12 @@
+--CUSTOMERS TABLOSUNA YAS GRUBU ICIN YENI BIR ALAN EKLEYINIZ,
+--BU ISLEMI HEM MANAGEMENT STUDIO ILE HEM DE SQL KODU ILE YAPINIZ.
+-- ALAN ADI : AGEGROUP, VERITIPI: VARCHAR(50)
+
+-- ilk soru, CUSTOMERS sað tikla, design'dan ekle.
+
+-- ikinci kýsým icin
+--(obje olustururken create,
+-- silerken drop,
+-- degistirirken de alter komutu)
+
+ALTER TABLE CUSTOMERS ADD AGEGROUP VARCHAR(50)
