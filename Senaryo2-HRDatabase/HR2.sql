a4b3bc24b2e173a75a07953f983925248fd3c0b6
Volkan Karakus <64658867+volkaankarakus@users.noreply.github.com>
2021-04-08 01:32:40 +0300

    Add files via upload

---
+++ Senaryo2-HRDatabase/HR2.sql
@@ -0,0 +1,10 @@
+--excel'den sql'e veri aktarirken daha onceden bir degisiklik varsa bunlari silmek icin tablonun ýcýný bosaltalim.
+TRUNCATE TABLE DEPARTMENT
+--sonra departmen uzerine gelip sag tik, edit top 200 diyoruz
+--buradan yukarýdaký SQL'e basip, top ID'yi siliyoruz.
+--------------------
+SELECT * FROM DEPARTMENT
+--------------------
+TRUNCATE TABLE POSITION
+--------------------
+SELECT * FROM POSITION
